2232a2a6f91dc4ed61a1dc497866652ce1679880
Nick Sanders <cogciprocate@gmail.com>
2017-03-10 23:16:59 -0800

    Add more public interfaces to internals.

--- src/qutex.rs
+++ src/qutex.rs
@@ -1,5 +1,8 @@
-//! A mutex-like lock which can be shared between threads.
+//! A queue-backed exclusive data lock.
 //!
+//
+// * It is unclear how many of the unsafe methods within need actually remain
+//   unsafe.
 
 use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
@@ -32,7 +35,7 @@ impl<T> DerefMut for Guard<T> {
 
 impl<T> Drop for Guard<T> {
     fn drop(&mut self) {
-        self.lock.unlock();
+        unsafe { self.lock.unlock() };
     }
 }
 
@@ -43,6 +46,13 @@ pub struct FutureGuard<T> {
 }
 
 impl<T> FutureGuard<T> {
+    fn new(lock: Qutex<T>, rx: oneshot::Receiver<()>) -> FutureGuard<T> {
+        FutureGuard {
+            lock: Some(lock),
+            rx: rx,
+        }
+    }
+
     pub fn wait(self) -> Result<Guard<T>, Canceled> {
         <Self as Future>::wait(self)
     }
@@ -55,7 +65,7 @@ impl<T> Future for FutureGuard<T> {
     #[inline]
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         if self.lock.is_some() {
-            self.lock.as_ref().unwrap().process_queue();
+            unsafe { self.lock.as_ref().unwrap().process_queue(); }
 
             match self.rx.poll() {
                 Ok(status) => Ok(status.map(|_| {
@@ -72,75 +82,15 @@ impl<T> Future for FutureGuard<T> {
 }
 
 
-////// [BRING ME BACK ONE DAY USING GENERICS]:
-// /// Like a `FutureGuard` but additionally waits on an OpenCL event.
-// pub struct PendingGuard<T> {
-//     lock: Option<Qutex<T>>,
-//     rx: oneshot::Receiver<()>,
-//     wait_event: Event,
-//     trigger_event: Event,
-//     // rx_is_complete: bool,
-//     // ev_is_complete: bool,
-// }
-
-// impl<T> PendingGuard<T> {
-//     fn new(lock: Option<Qutex<T>>, rx: oneshot::Receiver<()>, wait_event: Event) -> OclResult<PendingGuard<T>> {
-//         let trigger_event = Event::user(&wait_event.context()?)?;
-
-//         Ok(PendingGuard {
-//             lock: lock,
-//             rx: rx,
-//             wait_event: wait_event,
-//             trigger_event: trigger_event,
-//             // rx_is_complete: false,
-//             // ev_is_complete: false,
-//         })
-//     }
-
-//     pub fn trigger_event(&self) -> &Event {
-//         &self.trigger_event
-//     }
-
-//     pub fn wait(self) -> AsyncResult<Guard<T>> {
-//         <Self as Future>::wait(self)
-//     }
-
-//     pub unsafe fn cell_mut(&self) -> Option<&mut T> {
-//         self.lock.as_ref().map(|l| &mut *l.inner.cell.get())
-//     }
-// }
-
-// impl<T> Future for PendingGuard<T> {
-//     type Item = Guard<T>;
-//     type Error = AsyncError;
-
-//     #[inline]
-//     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-//         if self.lock.is_some() {
-//             self.lock.as_ref().unwrap().process_queue();
-
-//             if !self.wait_event.is_complete()? {
-//                 let task_ptr = standard::box_raw_void(task::park());
-//                     unsafe { self.wait_event.set_callback(standard::_unpark_task, task_ptr)?; };
-//                     return Ok(Async::NotReady);
-//             }
-
-//             match self.rx.poll() {
-//                 Ok(status) => Ok(status.map(|_| {
-//                     Guard { lock: self.lock.take().unwrap() }
-//                 })),
-//                 Err(e) => return Err(e.into()),
-//             }
-//         } else {
-//             Err("PendingGuard::poll: Task already completed.".into())
-//         }
-//     }
-// }
-
-
-struct Request {
+pub struct Request {
     tx: oneshot::Sender<()>,
     // wait_event: Option<Event>,
+}
+
+impl Request {
+    pub fn new(tx: oneshot::Sender<()>) -> Request {
+        Request { tx: tx }
+    }
 }
 
 
@@ -166,6 +116,7 @@ unsafe impl<T: Send> Send for Inner<T> {}
 unsafe impl<T: Send> Sync for Inner<T> {}
 
 
+/// A queue-backed exclusive data lock.
 pub struct Qutex<T> {
     inner: Arc<Inner<T>>,
 }
@@ -179,8 +130,65 @@ impl<T> Qutex<T> {
         }
     }
 
+    /// Returns a new `FutureGuard` which can be used as a future and will
+    /// resolve into a `Guard`.
+    pub fn lock(&self) -> FutureGuard<T> {
+        let (tx, rx) = oneshot::channel();
+        // self.inner.queue.push(Request { tx: tx, wait_event: None });
+        // self.inner.queue.push(Request { tx: tx });
+        unsafe { self.push_request(Request::new(tx)); }
+        // FutureGuard { lock: Some((*self).clone()), rx: rx }
+        FutureGuard::new((*self).clone(), rx)
+    }
+
+    /// Pushes a lock request onto the queue.
+    ///
+    //
+    // TODO: Evaluate unsafeness.
+    //
+    #[inline]
+    pub unsafe fn push_request(&self, req: Request) {
+        self.inner.queue.push(req);
+    }
+
+    /// Returns a mutable reference to the inner `Vec` if there are currently
+    /// no other copies of this `Qutex`.
+    ///
+    /// Since this call borrows the inner lock mutably, no actual locking needs to
+    /// take place---the mutable borrow statically guarantees no locks exist.
+    ///
+    #[inline]
+    pub fn get_mut(&mut self) -> Option<&mut T> {
+        Arc::get_mut(&mut self.inner).map(|inn| unsafe { &mut *inn.cell.get() })
+    }
+
+    /// Returns a reference to the inner value.
+    ///
+    /// This is frought with potential peril.
+    ///
+    #[inline]
+    pub fn as_ptr(&self) -> *const T {
+        self.inner.cell.get()
+    }
+
+    /// Returns a mutable reference to the inner value.
+    ///
+    /// Drinking water from the tap in 1850's London would be safer.
+    ///
+    #[inline]
+    pub fn as_mut_ptr(&self) -> *mut T {
+        self.inner.cell.get()
+    }
+
     /// Pops the next lock request in the queue if this lock is unlocked.
-    fn process_queue(&self) {
+    //
+    // TODO: Determine if this needs to be unsafe.
+    //  * This is currently public/unsafe due to 'derivers' (aka. sub-types).
+    //  * Consider removing unsafe qualifier.
+    //    - If this imposes a performance burden, create a 'safe' version with
+    //      necessary runtime checks
+    //
+    pub unsafe fn process_queue(&self) {
         match self.inner.state.compare_and_swap(0, 1, SeqCst) {
             // Unlocked:
             0 => {
@@ -197,40 +205,20 @@ impl<T> Qutex<T> {
         }
     }
 
-    /// Returns a new `FutureGuard` which can be used as a future and will
-    /// resolve into a `Guard`.
-    pub fn lock(&self) -> FutureGuard<T> {
-        let (tx, rx) = oneshot::channel();
-        // self.inner.queue.push(Request { tx: tx, wait_event: None });
-        self.inner.queue.push(Request { tx: tx });
-        FutureGuard { lock: Some((*self).clone()), rx: rx }
-    }
-
     /// Unlocks this lock and wakes up the next task in the queue.
-    fn unlock(&self) {
+    //
+    // TODO: Evaluate unsafeness.
+    pub unsafe fn unlock(&self) {
         // TODO: Consider using `Ordering::Release`.
         self.inner.state.store(0, SeqCst);
         self.process_queue();
-    }
-
-    /// Returns a mutable reference to the inner `Vec` if there are currently
-    /// no other copies of this `Qutex`.
-    ///
-    /// Since this call borrows the inner lock mutably, no actual locking needs to
-    /// take place---the mutable borrow statically guarantees no locks exist.
-    ///
-    #[inline]
-    pub fn get_mut(&mut self) -> Option<&mut T> {
-        Arc::get_mut(&mut self.inner).map(|inn| unsafe { &mut *inn.cell.get() })
     }
 }
 
 impl<T> From<T> for Qutex<T> {
     #[inline]
     fn from(val: T) -> Qutex<T> {
-        Qutex {
-            inner: Arc::new(Inner::from(val))
-        }
+        Qutex::new(val)
     }
 }
 
